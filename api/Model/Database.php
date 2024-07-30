3648f0996cffcd58acf9c29ba6135cf339199e40
abbas alkaabi <abbasalkabbi2003@gmail.com>
2024-07-30 05:21:29 +0300

    ADD =>DataBase

---
+++ api/Model/Database.php
@@ -0,0 +1,42 @@
+<?php 
+class db {
+    private $DB_HOST; 
+    private $DB_USERNAME; 
+    private $DB_PASSWORD; 
+    private $DB_DATABASE_NAME; 
+    private $DATABASE; 
+
+    public function Set($DataBase){
+        $this->DB_HOST=$DataBase->Host;
+        $this->DB_USERNAME=$DataBase->User;
+        $this->DB_PASSWORD=$DataBase->Password;
+        $this->DB_DATABASE_NAME=$DataBase->Name;
+    }
+    public function connect(){
+     // Create connection
+        $conn = mysqli_connect($this->DB_HOST,$this->DB_USERNAME, $this->DB_PASSWORD,$this->DB_DATABASE_NAME);
+        
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        echo "Connected successfully";
+        return $this->DATABASE=$conn;
+    }
+    public function get(){
+        return $this->DATABASE;
+    }
+
+}
+$run = new db();
+$run->Set((object) array(
+    "Host"=>'localhost',
+    "User"=>'root',
+    "Password"=>'',
+    "Name"=>'may-hwm',
+));
+$run->connect(); 
+$data=mysqli_query($run->get() ,'SELECT * FROM `users` ');
+var_dump( mysqli_fetch_all($data,MYSQLI_ASSOC));
+// var_dump( $run);
+?>
